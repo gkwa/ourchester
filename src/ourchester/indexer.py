c201e4dde9e2e2e3f39f57699c57ef32f7bb6529
Taylor Monacelli <taylormonacelli@gmail.com>
2024-05-26 08:55:40 -0700

    refactor

--- src/ourchester/indexer.py
+++ src/ourchester/indexer.py
@@ -11,36 +11,61 @@ def index_files(directories, index_dir, extensions):
         path=whoosh.fields.ID(unique=True, stored=True),
         content=whoosh.fields.TEXT(stored=True),
     )
+    ix = _create_or_open_index(index_dir, schema)
+    writer = ix.writer()
+    indexed_paths = _index_directories(directories, extensions, writer)
+    _remove_deleted_files(ix, indexed_paths, writer)
+    writer.commit()
+
+
+def _create_or_open_index(index_dir, schema):
     if not index_dir.exists():
         index_dir.mkdir()
-        ix = whoosh.index.create_in(str(index_dir), schema)
+        return whoosh.index.create_in(str(index_dir), schema)
     else:
-        ix = whoosh.index.open_dir(str(index_dir))
-    writer = ix.writer()
+        return whoosh.index.open_dir(str(index_dir))
+
+
+def _index_directories(directories, extensions, writer):
     indexed_paths = set()
     for directory in directories:
         for ext in extensions:
             file_pattern = f"**/*.{ext}"
             text_files = directory.glob(file_pattern)
             for file_path in text_files:
-                if file_path.is_symlink():
-                    try:
-                        file_path = file_path.resolve(strict=True)
-                    except FileNotFoundError:
-                        logger.warning(f"Broken symlink: {file_path}")
-                        continue
-                file_path_str = str(file_path)
-                indexed_paths.add(file_path_str)
-                with file_path.open(encoding="utf-8") as file:
-                    content = file.read()
-                writer.update_document(path=file_path_str, content=content)
-    to_delete = []
-    for doc in ix.searcher().documents():
-        if doc["path"] not in indexed_paths:
-            to_delete.append(doc["path"])
+                file_path_str = _resolve_file_path(file_path)
+                if file_path_str:
+                    indexed_paths.add(file_path_str)
+                    _index_file(file_path, file_path_str, writer)
+    return indexed_paths
+
+
+def _resolve_file_path(file_path):
+    if file_path.is_symlink():
+        try:
+            file_path = file_path.resolve(strict=True)
+            return str(file_path)
+        except FileNotFoundError:
+            logger.warning(f"Broken symlink: {file_path}")
+            return None
+    else:
+        return str(file_path)
+
+
+def _index_file(file_path, file_path_str, writer):
+    with file_path.open(encoding="utf-8") as file:
+        content = file.read()
+    writer.update_document(path=file_path_str, content=content)
+
+
+def _remove_deleted_files(ix, indexed_paths, writer):
+    to_delete = [
+        doc["path"]
+        for doc in ix.searcher().documents()
+        if doc["path"] not in indexed_paths
+    ]
     for path in to_delete:
         writer.delete_by_term("path", path)
-    writer.commit()
 
 
 def load_index(index_dir):
