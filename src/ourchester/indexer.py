687960b83f0076afa5d97bf8ff90911f65e06511
Taylor Monacelli <taylormonacelli@gmail.com>
2024-05-27 17:18:47 -0700

    refactor

--- src/ourchester/indexer.py
+++ src/ourchester/indexer.py
@@ -61,41 +61,5 @@ def _create_or_open_index(index_dir, schema):
         return whoosh.index.open_dir(str(index_dir))
 
 
-def _index_directories(directories, extensions, writer):
-    indexed_paths = set()
-    for directory in directories:
-        logger.info(f"Indexing directory: {directory}")
-        for ext in extensions:
-            file_pattern = f"**/*.{ext}"
-            text_files = directory.glob(file_pattern)
-            for file_path in text_files:
-                logging.debug(f"indexing {file_path}")
-                file_path_str = _resolve_file_path(file_path)
-                if file_path_str:
-                    logging.debug(f"deleting from whoosh index {file_path_str}")
-                    writer.delete_by_term("path", file_path_str)
-                    indexed_paths.add(file_path_str)
-                    _index_file(file_path, file_path_str, writer)
-    return indexed_paths
-
-
-def _resolve_file_path(file_path):
-    if file_path.is_symlink():
-        try:
-            file_path = file_path.resolve(strict=True)
-            return str(file_path)
-        except FileNotFoundError:
-            logger.warning(f"Broken symlink: {file_path}")
-            return None
-    else:
-        return str(file_path)
-
-
-def _index_file(file_path, file_path_str, writer):
-    with file_path.open(encoding="utf-8") as file:
-        content = file.read()
-    writer.update_document(path=file_path_str, content=content)
-
-
 def load_index(index_dir):
     return whoosh.index.open_dir(str(index_dir))
