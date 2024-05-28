659f34c1d2a09c0275543f675d21047b31dfbc07
Taylor Monacelli <taylormonacelli@gmail.com>
2024-05-28 07:14:13 -0700

    When searching don't limit to 10 results please

--- src/ourchester/searcher.py
+++ src/ourchester/searcher.py
@@ -9,5 +9,5 @@ def perform_proximity_search(ix, query_str):
     with ix.searcher() as searcher:
         query = whoosh.qparser.QueryParser("content", ix.schema).parse(query_str)
         logger.debug(f"Parsed query: {query}")
-        results = searcher.search(query)
+        results = searcher.search(query, limit=None)
         return [dict(hit) for hit in results]
